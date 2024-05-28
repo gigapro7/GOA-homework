0b3edf35469bd19228eb5f7f34958e4e608282e8
Giga <58662245+gigapro7@users.noreply.github.com>
2024-05-28 23:24:18 +0400

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,22 @@
+print(5 < 10)
+print(5 > 8)
+print(2 >= 25)
+print(3 == 2)
+print(2 == 5)
+print(9 >= 3)
+print(2 >= 8)
+print(2 <= 4)
+print(2 != 5)
+print(5 != 2)
+
+
+
+print(true and true)
+print(false and false)
+print(true and false)
+print(false and true)
+print(true or false)
+print(false or true)
+print(true or true)
+print(false or false)
+
